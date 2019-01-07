a613273e4100a977539e3ea427bd297861012cf5
ibrahimrahhal <ibrahim.rahhal3636@gmail.com>
2019-01-07 13:14:06 -0800

    some design changes

--- design.js
+++ design.js
@@ -1,3 +1,11 @@
 $(".output-btn").click(()=>{
   $(".output-text").toggleClass("output-text-active");
+setTimeout(()=>{
+  $(".output-btn").css("opacity","0");
+
+  $(".output-btn").css("height","0");
+  setTimeout(()=>{
+    $(".output-btn").css("display","none");
+  },300);
+},400);
 });
