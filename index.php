c077b91d639485ace9270d172f77549458b6fd05
ibrahimrahhal <ibrahim.rahhal3636@gmail.com>
2019-01-12 02:44:54 -0800

    side bar design

--- index.php
+++ index.php
@@ -27,16 +27,24 @@
 <div class="all">
   <div class="side-menu">
     <div class="side-menu-title">
+      <div class="side-menu-title-icon">
+
+      </div>
       قائمة المحتويات
     </div>
-    <div class="side-menu-item">
+    <div class="side-menu-items">
+
+
+    <div class="side-menu-item hvr-underline-from-center">
       محتوى
     </div>
-    <div class="side-menu-item">
+    <div class="side-menu-item hvr-underline-from-center">
       محتوى
     </div>
-    <div class="side-menu-item">
+    <div class="side-menu-item hvr-underline-from-center">
+
       محتوى
+    </div>
     </div>
   </div>
   <div class="tut-content">
